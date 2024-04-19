2e345684426d86b0bac57d320f298b728ffca1d3
Sebastian <svaldes04@gmail.com>
2024-04-19 02:07:30 +0100

    Personal project initial commit + minor changes

--- Create with Code Part 1 Mission 1 - Player Control/Prototype 1/Assets/Scripts/PlayerController.cs
+++ Create with Code Part 1 Mission 1 - Player Control/Prototype 1/Assets/Scripts/PlayerController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private float speed = 20.0f;
+    private float speed = 40.0f;
     private float turnSpeed = 80.0f;
     
     private float horizontalInput;
