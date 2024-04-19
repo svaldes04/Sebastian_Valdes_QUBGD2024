c95cfeeceaf3e3c8a50772d812c9fdce20e00510
Sebastian <svaldes04@gmail.com>
2024-04-19 11:06:19 +0100

    Mission 1 Unit 2 Challenge

---
+++ Create with Code Part 1 Mission 2 - Basic Gameplay/Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float cooldown = 0.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (cooldown <= 0.0f && Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            cooldown = 1.0f;
+        }
+
+        if(cooldown > 0.0f)
+        {
+            cooldown -= Time.deltaTime;
+        }
+    }
+}
